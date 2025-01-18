ae0277f9dff5e2deca5e49ab7cd9a67879d7143a
BC0Pants <bc0pants@gmail.com>
2025-01-18 15:48:18 +0200

    Added main layout and added about page

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import handlebars from "express-handlebars";
 
 const app = express();
 
-app.engine('hbs',handlebars.engine({
-    extname:'hbs',
-}));
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
 
-app.set('view engine','hbs');
-app.set('views','./src/views');
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
 
-app.use('/static',express.static('src/public'))
+app.use("/static", express.static("src/public"));
 
-app.get('/',(req,res)=>[
-    res.render('home',{layout:false})
-])
-
-app.listen(5000, console.log('The app is running at http://localhost:5000...'));
+app.get("/", (req, res) => [res.render("home")]);
+app.get("/about", (req, res) => [res.render("about")]);
+app.listen(5000, console.log("The app is running at http://localhost:5000..."));
